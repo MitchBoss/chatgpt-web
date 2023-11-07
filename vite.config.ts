7a086928f5b21c29f06b12a9e4f0cca4f9c098c1
MitchBoss <42152605+MitchBoss@users.noreply.github.com>
2023-11-08 04:39:02 +1100

    dev & build can have different base url
    If you use custom domains on a root directory for GitHub pages the base needs to be set to './' but that breaks functionality on localhost.
    
    This gives a user the option to set up different options for build and dev.

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,34 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import dsv from '@rollup/plugin-dsv'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import dsv from '@rollup/plugin-dsv';
 
-import purgecss from '@fullhuman/postcss-purgecss'
+import purgecss from '@fullhuman/postcss-purgecss';
 
-const plugins = [svelte(), dsv()]
+const plugins = [svelte(), dsv()];
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
+  // Base configuration that applies to both build and dev
+  const config = {
+    plugins,
+  };
+
   // Only run PurgeCSS in production builds
   if (command === 'build') {
-    return {
-      plugins,
-      css: {
-        postcss: {
-          plugins: [
-            purgecss({
-              content: ['./**/*.html', './**/*.svelte'],
-              safelist: ['pre', 'code']
-            })
-          ]
-        }
+    config.css = {
+      postcss: {
+        plugins: [
+          purgecss({
+            content: ['./**/*.html', './**/*.svelte'],
+            safelist: ['pre', 'code'],
+          }),
+        ],
       },
-      base: './'
-    }
+    };
+    config.base = '/'; // Base set for build - change to './' if accessed on root URL
   } else {
-    return {
-      plugins
-    }
+    config.base = '/'; // Base set for dev
   }
-})
+
+  return config;
+});
